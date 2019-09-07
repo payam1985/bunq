1714ec032f5979193b91f7f9cc9b271ac90ded7c
payam <payam.broumand@yahoo.com>
2019-09-07 12:44:10 +0430

    complete payment repository

---
+++ DomainCore/IPaymentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataModel;
+
+namespace DomainCore
+{
+    public interface IPaymentRepository
+    {
+        Task<Payment> GetPayment(long id);
+
+        Task<IEnumerable<Payment>> GetUserPayments(long userId);
+
+        Task<Payment> Create(Payment payment);
+
+        Task<Payment> Update(Payment payment);
+
+        Task<int> Delete(long id);
+    }
+}
--- Repository.cs/PaymentRepository.cs
+++ Repository.cs/PaymentRepository.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataModel;
+using DomainCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace Repository
 {
-    public class PaymentRepository
+    public class PaymentRepository : IPaymentRepository
     {
-        public long Id { get; set; }
-        public string Description { get; set; }
-        public DateTime Date { get; set; }
-        public decimal Amount { get; set; }
-        public string Recipient { get; set; }
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PaymentRepository(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Payment> GetPayment(long id) => await _unitOfWork.Context.Payments.FindAsync(id);
+
+        public async Task<IEnumerable<Payment>> GetUserPayments(long userId)
+            => await _unitOfWork.Context.Payments.Where(a => a.UserId == userId).ToListAsync();
+
+        public async Task<Payment> Create(Payment payment)
+        {
+            _unitOfWork.Context.Payments.Add(payment);
+            await _unitOfWork.Commit();
+            return payment;
+        }
+
+        public async Task<Payment> Update(Payment payment)
+        {
+            _unitOfWork.Context.Entry(payment).State = EntityState.Modified;
+            await _unitOfWork.Commit();
+            return payment;
+        }
+
+        public async Task<int> Delete(long id)
+        {
+            var payment = await GetPayment(id);
+            return payment == null ? 0 : await DeletePayment(payment);
+        }
+
+        private async Task<int> DeletePayment(Payment payment)
+        {
+            if (_unitOfWork.Context.Entry(payment).State == EntityState.Detached)
+                _unitOfWork.Context.Payments.Attach(payment);
+            _unitOfWork.Context.Payments.Remove(payment);
+            return await _unitOfWork.Commit();
+        }
     }
 }
