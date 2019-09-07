4780aae561be48d65a4746791e544b6d80ddc49c
payam <payam.broumand@yahoo.com>
2019-09-07 18:15:14 +0430

    add update user to UserController

--- Bunq/Controllers/UserController.cs
+++ Bunq/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataModel;
 using DomainCore;
 using Microsoft.AspNetCore.Mvc;
 
@@ -26,6 +27,31 @@ namespace Bunq.Controllers
             return Ok(users);
         }
 
+        public async Task<IActionResult> GetUser(long? id)
+        {
+            if (id == null || id == 0) return NotFound("user not found");
+            var user = await _userRepository.GetUser(id ?? 0);
+            if (user != null) return Ok(user);
+            return NotFound("user not found");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] User user)
+        {
+            var newUser = await _userRepository.Create(user);
+            if (newUser != null) return Ok(user);
+            ModelState.AddModelError(string.Empty, "an error occured while adding new user");
+            return BadRequest(ModelState);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Edit([FromBody] User user)
+        {
+            var editedUser = await _userRepository.Update(user);
+            if (editedUser != null) return Ok(editedUser);
+            ModelState.AddModelError(string.Empty, "Failed to update user");
+            return BadRequest(ModelState);
+        }
 
     }
 }
