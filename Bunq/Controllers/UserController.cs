785a2dc19a4e14aadfe0b1383d4741cb56d8787e
payam <payam.broumand@yahoo.com>
2019-09-07 17:43:26 +0430

    add route attribute to user controller

---
+++ Bunq/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bunq.Controllers
+{
+    [Route("api/User")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userRepository.GetUsers();
+            return Ok(users);
+        }
+
+
+    }
+}
